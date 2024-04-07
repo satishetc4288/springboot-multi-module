8b6be2023d91312f6a30dc3c76c26ac906d53de2
satish rajput <satishetc4288@gmail.com>
2024-04-07 15:16:35 +0530

    added mysql

--- spring-batch/src/main/resources/schema-all.sql
+++ spring-batch/src/main/resources/schema-all.sql
@@ -1,19 +1,9 @@
-DROP TABLE coffee IF EXISTS;
+DROP TABLE coffee;
 
 CREATE TABLE coffee  (
-    coffee_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    coffee_id int NOT NULL AUTO_INCREMENT,
     brand VARCHAR(20),
     origin VARCHAR(20),
-    characteristics VARCHAR(30)
-);
-
-DROP TABLE medicine IF EXISTS;
-
-CREATE TABLE medicine  (
-    med_id VARCHAR(36) PRIMARY KEY,
-    name VARCHAR(30),
-    type VARCHAR(30),
-    expiration_date TIMESTAMP,
-    original_price DECIMAL,
-    sale_price DECIMAL
+    characteristics VARCHAR(30),
+    PRIMARY KEY (coffee_id)
 );
