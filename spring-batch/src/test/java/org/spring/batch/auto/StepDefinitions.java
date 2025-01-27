b90920e477fb8255ab8bd7ab5663ab4e8a8621d3
satish rajput <satishetc4288@gmail.com>
2025-01-27 20:49:09 +0530

    spring jpa ammend

--- spring-batch/src/test/java/org/spring/batch/auto/StepDefinitions.java
+++ spring-batch/src/test/java/org/spring/batch/auto/StepDefinitions.java
@@ -1,13 +1,12 @@
 package org.spring.batch.auto;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,9 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StepDefinitions {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate testRestTemplate;
 
     private ResponseEntity<String> response;
+
+    @Before
+    public void before() {
+        testRestTemplate = testRestTemplate.withBasicAuth("admin", "admin");
+    }
 
     @Given("the Spring Boot application is running")
     public void the_spring_boot_application_is_running() {
@@ -26,15 +30,17 @@ public class StepDefinitions {
 
     @When("I send a GET request to {string}")
     public void i_send_a_get_request_to(String endpoint) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth("admin", "admin");
-        HttpEntity<String> request = new HttpEntity<>(headers);
-        response =  restTemplate.exchange(endpoint, HttpMethod.GET, request, String.class);
+        response =  testRestTemplate.getForEntity(endpoint, String.class);
     }
 
     @Then("the response should be {string}")
     public void the_response_should_be(String expectedResponse) {
         assertEquals(expectedResponse, response.getBody());
+    }
+
+    @And("the statuscode should be {int}")
+    public void the_statuscode_should_be(int expectedStatusCode) {
+        assertEquals(expectedStatusCode, response.getStatusCode().value());
     }
 }
 
--- spring-batch/src/test/resources/features/sample.feature
+++ spring-batch/src/test/resources/features/sample.feature
@@ -4,3 +4,4 @@ Feature: Sample Spring Boot API Test
     Given the Spring Boot application is running
     When I send a GET request to "/batch/hello"
     Then the response should be "Hello, World!"
+    And the statuscode should be 200
